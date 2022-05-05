6e083824132227121a0937a2f290eee0f749a9f5
Michelle Patiño Campos <thocokun@gmail.com>
2022-05-04 22:52:15 -0500

    Cotizaciones falta data timing

--- lib/Cotizaciones/ui/screens/cotizaciones.screen.dart
+++ lib/Cotizaciones/ui/screens/cotizaciones.screen.dart
@@ -5,6 +5,144 @@ class CotizacionesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var onPressed;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 4,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(
+              top: 30,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.red,
+            ),
+            child: const Text(
+              'Crear una cotizacion',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.only(
+                bottom: 30,
+                top: 30,
+              ),
+              height: MediaQuery.of(context).size.height,
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 5,
+                left: 4,
+                right: 4,
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const TextField(
+                      // controller: costoController,
+                      decoration: InputDecoration(
+                        hintText: '0.00',
+                        labelText: 'Costo Mano de obra',
+                        prefixIcon: Icon(Icons.monetization_on_outlined),
+                        //icon: Icon(Icons.monetization_on),
+                        /*  suffixIcon: IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () => costoController.clear(),
+                        ),*/
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const TextField(
+                      //controller: _costoController,
+                      decoration: InputDecoration(
+                        hintText: '0.00',
+                        labelText: 'Costo  Materiales',
+                        prefixIcon: Icon(Icons.monetization_on_outlined),
+                        //icon: Icon(Icons.monetization_on),
+                        /*   suffixIcon: IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () => _costoController.clear(),
+                        ),*/
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const TextField(
+                      // controller: costoController,
+                      decoration: InputDecoration(
+                        hintText: '0.00',
+                        labelText: 'Total',
+                        prefixIcon: Icon(
+                          Icons.monetization_on_sharp,
+                          color: Colors.yellow,
+                        ),
+                        //icon: Icon(Icons.monetization_on),
+                        /*  suffixIcon: IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () => costoController.clear(),
+                        ),*/
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Descripcion del problema',
+                          labelStyle: TextStyle(fontSize: 20),
+                          hintText: 'Descripcion',
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Color.fromARGB(71, 226, 208, 208),
+                          hintMaxLines: 50,
+                          contentPadding: EdgeInsets.all(50.0)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(
+                            top: 5,
+                            left: 230,
+                            right: 5,
+                          ),
+                          child: ElevatedButton(
+                              child: Text("Guardar"), onPressed: () {}),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/navbar.dart
+++ lib/navbar.dart
@@ -41,7 +41,7 @@ class _BasicBottomNavBarState extends State<BasicBottomNavBar> {
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.camera),
-              label: 'Camera',
+              label: 'Cotizacion',
             ),
           ],
           currentIndex: _selectedIndex,
