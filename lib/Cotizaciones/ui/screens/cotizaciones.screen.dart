6ec38bb5e34f3bd73e2b1183d8daf19f8e9745de
Michelle Patiño Campos <thocokun@gmail.com>
2022-05-06 00:35:55 -0500

    Picket image &&& revisar plis o les va a salir error xd

--- lib/Cotizaciones/ui/screens/cotizaciones.screen.dart
+++ lib/Cotizaciones/ui/screens/cotizaciones.screen.dart
@@ -9,6 +9,86 @@ class CotizacionesScreen extends StatelessWidget {
   final firstDate = DateTime(2010, 1);
   final lastDate = DateTime(2022, 12);
 */
+  opciones(context){
+    showDialog(
+      context: context,
+       builder: (BuildContext context){
+         return AlertDialog(
+           contentPadding: EdgeInsets.all(0),
+           content: SingleChildScrollView(
+             child: Column(
+               children: [
+                 InkWell(
+                   onTap: (){
+
+                   },
+                   child: Container(
+                     padding: EdgeInsets.all(20),
+                     decoration: BoxDecoration(
+                       border: Border (bottom: BorderSide(
+                         width: 1,
+                         color: Colors.grey
+                       ))
+                     ),
+                     child: Row(
+                       children: [
+                         Expanded(
+                         child: Text("Tomar una foto", 
+                         style: TextStyle(fontSize: 16
+                         ),),
+                         ),
+                         Icon(Icons.camera_alt, color: Colors.blue,)
+                       ]),
+                   ),
+                 ),
+                       InkWell(
+                   onTap: (){
+
+                   },
+                   child: Container(
+                     padding: EdgeInsets.all(20),
+                     child: Row(
+                       children: [
+                         Expanded(
+                         child: Text("Seleccionar una foto", 
+                         style: TextStyle(fontSize: 16
+                         ),),
+                         ),
+                         Icon(Icons.image, color: Colors.blue,)
+                       ]),
+                   ),
+                 ),
+                   InkWell(
+                   onTap: (){
+                     Navigator.of(context).pop();
+
+                   },
+                   child: Container(
+                     padding: EdgeInsets.all(20),
+                     decoration: BoxDecoration(
+                       color: Colors.red
+                     ),
+                     child: Row(
+                       children: [
+                         Expanded(
+                         child: Text("Cancelar", 
+                         style: TextStyle(fontSize: 16,
+                         color: Colors.white,
+                         ),
+                         textAlign: TextAlign.center),
+                         ),
+                         
+                         Icon(Icons.close, color: Colors.grey,)
+                       ]),
+                   ),
+                 ),
+               ],
+             )
+           ),
+         ),
+       },),
+  }
+
   @override
   Widget build(BuildContext context) {
     var date = null.obs;
@@ -185,7 +265,10 @@ class CotizacionesScreen extends StatelessWidget {
                                 width: 125,
                               ),
                               ElevatedButton(
-                                  child: Text("Guardar"), onPressed: () {}),
+                                  child: Text("Subir"),
+                                  onPressed: () {
+                                    opciones(context);
+                                  }),
                               SizedBox(
                                 width: 15,
                               ),
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static const String API_URL = 'http://192.168.1.13:4000';
+  static const String API_URL = 'http://192.168.1.5:4000';
 }
