bb1cc2402a004bbc50ab99d72dde667c462cf515
Maria Viridiana Garcia Martinez <viriidiana089@gmail.com>
2022-05-05 19:24:01 -0500

    Auto stash before merge of "main" and "origin/main"

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,3 @@
 class Constants {
-<<<<<<< Updated upstream
-  static const String API_URL = 'http://192.168.1.12:4000';
-=======
   static const String API_URL = 'http://192.168.1.13:4000';
->>>>>>> Stashed changes
 }
