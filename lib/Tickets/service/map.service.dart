0611ff65a6bc46496ff80eb84e42b6b39aa7b7e5
Emmanuel <flotuz10@gmail.com>
2022-06-16 09:51:09 -0500

    Navegacion por coordenadas

--- lib/Tickets/service/map.service.dart
+++ lib/Tickets/service/map.service.dart
@@ -2,28 +2,39 @@ import 'package:grupoLias/Tickets/model/ticket.model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Mapita {
-  Future<void> abrirMapa(Ticket ticket) async {
-    var calle = ticket.calle.toString();
-    var colonia = ticket.colonia.toString();
-    var ciudad = ticket.ciudadId.toString();
-    var numero = ticket.numeroDomicilio.toString();
+  Future<void> abrirMapa(Ticket? ticket) async {
+    var calle = ticket?.calle.toString();
+    var colonia = ticket?.colonia.toString();
+    var ciudad = ticket?.ciudadId.toString();
+    var numero = ticket?.numeroDomicilio.toString();
 
-    calle = calle.replaceAll(" ", "+");
-    colonia = colonia.replaceAll(" ", "+");
-    ciudad = ciudad.replaceAll(" ", "+");
-    numero = numero.replaceAll(" ", "+");
+    calle = calle?.replaceAll(" ", "+");
+    colonia = colonia?.replaceAll(" ", "+");
+    ciudad = ciudad?.replaceAll(" ", "+");
+    numero = numero?.replaceAll(" ", "+");
 
     var direccion = "$calle+$colonia+$numero+$ciudad";
 
-    Uri googleUrl = Uri.parse('google.navigation:q=$direccion&avoid=tf');
+    bool? isAsistenciaVial = ticket?.asistenciaVial;
 
-    if (await canLaunchUrl(googleUrl)) {
-      // await launchUrl(
-      //   googleUrl,
-      //   mode: LaunchMode.externalApplication,
-      // );
+    if (isAsistenciaVial!) {
+      Uri url = Uri.parse('google.navigation:q=$calle&avoid=tf');
+      if (await canLaunchUrl(url)) {
+        await launchUrl(url);
+      } else {
+        throw 'No se pudo abrir el mapa';
+      }
     } else {
-      throw 'Could not open the map.';
+      Uri googleUrl = Uri.parse('google.navigation:q=$direccion&avoid=tf');
+
+      if (await canLaunchUrl(googleUrl)) {
+        await launchUrl(
+          googleUrl,
+          mode: LaunchMode.externalApplication,
+        );
+      } else {
+        throw 'Could not open the map.';
+      }
     }
   }
 }
